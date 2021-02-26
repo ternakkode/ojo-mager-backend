68664ecea060da01d2befe322af307106cbc02e5
M. Firhan Azmi Nor <muhammad.firhan.azmi@gmail.com>
2021-02-26 18:55:34 +0700

    FIX | Error on error handling middleware when not thrown from ApiErrorHandler

--- middleware/errorHandlingApi.js
+++ middleware/errorHandlingApi.js
@@ -1,11 +1,18 @@
+const ApiErrorHandler = require('../helpers/ApiErrorHandler');
 const { failedApi } = require('../utils/response')
 
 const errorHandlingApi = async (err, req, res, next) => {
-    const { statusCode, message, data } = err
-
-    res.status(statusCode).json(
-        failedApi(statusCode, message, data)
-    );
+    if (err instanceof ApiErrorHandler) {
+        const { statusCode, message, data } = err
+        
+        res.status(statusCode).json(
+            failedApi(statusCode, message, data)
+        );
+    } else {
+        res.status(500).json(
+            failedApi(500, "something went wrong")
+        );
+    }
 }
 
 module.exports = errorHandlingApi;
