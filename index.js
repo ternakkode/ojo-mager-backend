c2995f15f3080b41e8880a02fba63aa58ee3b6ee
M. Firhan Azmi Nor <muhammad.firhan.azmi@gmail.com>
2021-03-11 15:40:55 +0700

    fix | cors handler

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const apiRoutes = require('./routes/api');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({origin: '*'}));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
