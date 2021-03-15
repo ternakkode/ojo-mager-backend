fac8fbff4aaf63f529d4199e369faf70a0511574
M. Firhan Azmi Nor <muhammadfirhan@student.ub.ac.id>
2021-03-15 04:40:42 +0000

    Fix/unhandled errror

--- middleware/isVerified.js
+++ middleware/isVerified.js
@@ -2,16 +2,20 @@ const ApiErrorHandler = require('../helpers/ApiErrorHandler');
 const wording = require('./../utils/wording');
 
 const isVerified = async (req, res, next) => {
-    const { user } = req;
-    if (!user) {
-        throw new ApiErrorHandler(404, 'user not found');
-    }
+    try {
+        const { user } = req;
+        if (!user) {
+            throw new ApiErrorHandler(404, 'user not found');
+        }
+        
+        if (!user.is_verified) {
+            throw new ApiErrorHandler(403, wording.USER_NEED_EMAIL_VERIFICATION);
+        }
 
-    if (!user.is_verified) {
-        throw new ApiErrorHandler(403, wording.USER_NEED_EMAIL_VERIFICATION);
+        next();
+    } catch (err) {
+        next(err);
     }
-
-    next();
 }
 
 module.exports = isVerified;
